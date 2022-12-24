b6c7807234270c8509996286910814d6683a1d06
Rakesh <rakesh.das@mindfiresolutions.com>
2022-12-25 00:38:34 +0530

    First Commit

---
+++ src/main/java/project/streetfoodreview/StreetfoodreviewApplication.java
@@ -0,0 +1,13 @@
+package project.streetfoodreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreetfoodreviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreetfoodreviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/streetfoodreview/StreetfoodreviewApplicationTests.java
@@ -0,0 +1,13 @@
+package project.streetfoodreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreetfoodreviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
